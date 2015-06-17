9819a48a2daf2288ff69892401d8447cee8c9cfc
Marcos Vinicius Santini <msantini@ciandt.com>
2015-06-17 11:46:39 -0300

    tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   sequence(:email){|n| "person#{n}@example.com" }
 
   factory :user do
-    email "marcos@marcos.com"
+    email { Faker::Internet.email }
     password "12345678"
     password_confirmation "12345678"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  
+  context "when duplicated" do
+    it "should not have same emails" do
+      binding.pry
+      expect(user.email).not_to eq other_user.email
+    end
+  end
+
+  context "when new" do
+    let(:new_user) { "Ernane" }
+    it "assings a new plan" do
+      #expect(user.email).to eq "marcos@marcos.com"
+    end
+  end
 end
